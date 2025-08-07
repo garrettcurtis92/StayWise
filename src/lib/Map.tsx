a0673c5b914ddb61d1b47a5229e5fb34dd1a0678
Garrett Curtis <gcurtis1092@gmail.com>
2025-08-07 10:56:50 -0600

    Temporarily remove marker from map

--- src/lib/Map.tsx
+++ src/lib/Map.tsx
@@ -1,37 +1,22 @@
-// src/lib/Map.tsx
 "use client";
+import Map from "react-map-gl/mapbox";
 
-import React from "react";
-import ReactMapGL, { Marker } from "react-map-gl/mapbox";
-
-type MapProps = {
+interface MapProps {
   latitude: number;
   longitude: number;
   zoom?: number;
-};
+}
 
-export default function Map({ latitude, longitude, zoom = 10 }: MapProps) {
-  const [viewport, setViewport] = React.useState({
-    latitude,
-    longitude,
-    zoom,
-  });
-
+export default function ListingMap({ latitude, longitude, zoom = 10 }: MapProps) {
   return (
-    <div className="w-full h-64">
-      <ReactMapGL
-        {...viewport}
-        style={{ width: '100%', height: '100%' }}
-        onMove={({ viewState }) => setViewport({
-          latitude: viewState.latitude,
-          longitude: viewState.longitude,
-          zoom: viewState.zoom
-        })}
+    <div className="relative w-full h-64">
+      <Map
+        initialViewState={{ latitude, longitude, zoom }}
         mapStyle="mapbox://styles/mapbox/streets-v11"
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-      >
-        <Marker latitude={latitude} longitude={longitude} />
-      </ReactMapGL>
+        attributionControl={false}
+        style={{ width: "100%", height: "100%" }}
+      />
     </div>
   );
 }
